59164cf3aef06b0d7965561efe846f97434e11d7
Rijwanul-Haque-Roman <rizwanul210058@diit.edu.bd>
2024-02-28 12:05:15 +0600

    added feature details modal

--- js/phones.js
+++ js/phones.js
@@ -52,7 +52,7 @@ const displayPhone = (phones, isShowAll) => {
         suffered
     </p>
     <h3 class="text-[h-clr] text-[25px] font-bold">$999</h3>
-    <button class=" btn text-[20px] text-white font-bold bg-[#0D6EFD] hover:bg-blue-700 px-8">Show
+    <button onclick="showDetails('${phone["slug"]}'), details.showModal()" class=" btn text-[20px] text-white font-bold bg-[#0D6EFD] hover:bg-blue-700 px-8">Show
         Details</button>
     `;
     phoneContainer.appendChild(phoneCard);
@@ -76,8 +76,70 @@ const toggleLoadingSpinner = (isLoading) => {
   }
 };
 
+const showDetails = async (id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await response.json();
+  const details = data.data;
+  console.log(details);
+
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = `
+  <img class="mx-auto" src="${details.image}" alt="">
+                <h3 class="font-bold text-2xl text-[h-clr]">${details.name}</h3>
+                <p class="py-4">It is a long established fact that a reader will be distracted by the readable content
+                    of a page when looking at its layout.</p>
+                <p><span class="text-base text-[h-clr]  font-semibold">storage:</span> ${details.mainFeatures.storage} </p>
+                <p><span class="text-base text-[h-clr]  font-semibold">displaySize:</span>${details.mainFeatures.displaySize}</p>
+                <p><span class="text-base text-[h-clr]  font-semibold">chipSet:</span>${details.mainFeatures.chipSet}</p>
+                <p><span class="text-base text-[h-clr]  font-semibold">memory:</span>${details.mainFeatures.memory}</p>
+                <p><span class="text-base text-[h-clr]  font-semibold">brand:</span>${details.brand}</p>
+                <div class="modal-action">
+                    <form method="dialog">
+                        <button class="btn bg-[#DC3545] text-white">Close</button>
+                    </form>
+                </div>
+  `;
+};
+
 const showAll = () => {
   handleSearch(true);
 };
 
 fetchData();
+
+/**
+ * {
+    "status": true,
+    "data": {
+        "mainFeatures": {
+            "storage": "128GB/256GB/1TB storage, no card slot",
+            "displaySize": "6.1 inches, 90.2 cm2 (~86.0% screen-to-body ratio)",
+            "chipSet": "Apple A15 Bionic (5 nm)",
+            "memory": "128GB 6GB RAM, 256GB 6GB RAM, 512GB 6GB RAM, 1TB 6GB RAM",
+            "sensors": [
+                "Face ID",
+                "accelerometer",
+                "gyro",
+                "proximity",
+                "compass",
+                "barometer"
+            ]
+        },
+        "slug": "apple_iphone_13_pro-11102",
+        "name": "iPhone 13 Pro",
+        "releaseDate": "",
+        "brand": "Apple",
+        "image": "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg",
+        "others": {
+            "WLAN": "Wi-Fi 802.11 a/b/g/n/ac/6, dual-band, hotspot",
+            "Bluetooth": "5.0, A2DP, LE",
+            "GPS": "Yes, with A-GPS, GLONASS, GALILEO, BDS, QZSS",
+            "NFC": "Yes",
+            "Radio": "No",
+            "USB": "Lightning, USB 2.0"
+        }
+    }
+}
+*/
